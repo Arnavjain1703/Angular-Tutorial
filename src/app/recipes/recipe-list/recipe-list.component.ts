ff8eebfe20a0153f959262d488bcb3946dc69e2e
Arnavjain1703 <arnavjainkht@gmail.com>
2019-09-02 11:08:17 +0530

    recipe project, nav bar ,new module

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipies:Recipe[]=[
+  new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg')
+];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
