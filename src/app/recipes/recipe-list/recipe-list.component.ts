f943cb2fb29d5b34073a61ab87df2ebbcfc09f38
Arnavjain1703 <arnavjainkht@gmail.com>
2019-09-03 21:52:38 +0530

    connect recipedetails to recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
 recipies:Recipe[]=[
   new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg'),
   new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg'),
@@ -15,5 +16,7 @@ recipies:Recipe[]=[
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe:Recipe){
+       this.recipeWasSelected.emit(recipe);
+  }
 }
