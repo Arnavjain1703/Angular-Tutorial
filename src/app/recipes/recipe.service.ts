cf51c0c86ecd1389d23d56722137ed48e8bc979a
Arnavjain1703 <arnavjainkht@gmail.com>
2019-09-06 13:39:52 +0530

    click button and add content to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,31 @@
 import {Recipe} from './recipe.model'
-import { EventEmitter } from '@angular/core';
- 
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+ @Injectable ()
 export class RecipeService{
     recipeSelected=new EventEmitter<Recipe>();
    private  recipes:Recipe[]=[
-        new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg'),
-        new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg'),
+        new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg',[
+            new Ingredient ('tikki' ,10),
+            new Ingredient ('paneer',2)
+
+        ]),
+        new Recipe('A test Recipe','this is simple test,','http://foodtalk4you.com/wp-content/uploads/2017/01/Recipes-Banner.jpg',[
+            
+            new Ingredient ('tikki' ,10),
+            new Ingredient ('paneer',2)
+  
+        ]),
       ];  
+      constructor(private slService:ShoppingListService){}
       getRecipes()
       {
           return this.recipes.slice();
       }
-}
+      addIngredientsToShoppingLIst(ingredients:Ingredient[])
+      {
+          this.slService.addIngredients(ingredients);
+      }
+}      
