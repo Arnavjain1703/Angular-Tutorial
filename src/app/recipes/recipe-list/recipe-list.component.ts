699cf4611388e2d3040a4fbddd71fea46efd73e3
Arnavjain1703 <arnavjainkht@gmail.com>
2019-09-06 22:50:31 +0530

    new recipe button working and calling to new

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,7 +11,9 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   // @Output() recipeWasSelected=new EventEmitter<Recipe>();
 recipes:Recipe[];
-  constructor( private recipeService:RecipeService) { }
+  constructor( private recipeService:RecipeService,
+                private router:Router,
+                private route :ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes=this.recipeService.getRecipes();
@@ -18,4 +21,7 @@ recipes:Recipe[];
   // onRecipeSelected(recipe:Recipe){
   //      this.recipeWasSelected.emit(recipe);
   // }
+  onNewRecipe(){
+    this.router.navigate(['/recipes','new']) ;
+  }
 }
